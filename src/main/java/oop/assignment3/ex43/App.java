75b55c55d26373f895ca6252ac7306a4c059e67b
savosburn <savosburn18@tampabay.rr.com>
2021-06-17 20:25:06 -0400

    incomplete

--- src/main/java/oop/assignment3/ex43/App.java
+++ src/main/java/oop/assignment3/ex43/App.java
@@ -5,5 +5,36 @@
 
 package oop.assignment3.ex43;
 
+/*
+Programming languages can create files and folders too.
+
+Create a program that generates a website skeleton with the following specifications:
+
+Prompt for the name of the site.
+Prompt for the author of the site.
+Ask if the user wants a folder for JavaScript files.
+Ask if the user wants a folder for CSS files.
+Generate an index.html file that contains the name of the site
+inside the <title> tag and the author in a <meta> tag.
+
+Example Output
+Site name: awesomeco
+Author: Max Power
+Do you want a folder for JavaScript? y
+Do you want a folder for CSS? y
+Created ./website/awesomeco
+Created ./website/awesomeco/index.html
+Created ./website/awesomeco/js/
+Created ./website/awesomeco/css/
+
+The user should then find these files and directories created in the working directory of your program.
+ */
+
 public class App {
+    public static void main(String[] args) {
+        // prompt for website
+        // prompt for author
+        // ask if you want javascript folder
+        // ask if you want a css folder
+    }
 }
