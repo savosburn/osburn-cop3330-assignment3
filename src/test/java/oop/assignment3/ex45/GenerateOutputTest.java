8cb36c9c4f0cbddeb3e035b9816f39d5b95c362b
savosburn <savosburn18@tampabay.rr.com>
2021-06-20 00:14:54 -0400

    GenerateOutput Tested

---
+++ src/test/java/oop/assignment3/ex45/GenerateOutputTest.java
@@ -0,0 +1,26 @@
+package oop.assignment3.ex45;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerateOutputTest {
+
+    @Test
+    void createOutputString(){
+
+        BuildParagraph paragraphBuilder = new BuildParagraph();
+        WordFinder finder = new WordFinder();
+        GenerateOutput output = new GenerateOutput();
+
+        String expected = "One should never use the word \"use\" in writing. Use \"use\" instead. For example, \"" +
+                "She uses an IDE to write her Java programs\" instead of \"She uses an IDE to write her Java programs\". ";
+
+        ArrayList<String> paragraph = finder.modifyList(paragraphBuilder.readFile("exercise45_input.txt"));
+
+        assertEquals(expected, output.createOutputString(paragraph));
+    }
+
+}
